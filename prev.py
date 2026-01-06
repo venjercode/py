a4f9b27fc844bba0e9e58ea59844d6a8f180f656
Sahil <52261334+venjercode@users.noreply.github.com>
2026-01-06 22:06:05 +0000

    new

---
+++ prev.py
@@ -0,0 +1,9 @@
+#Print the Sum of a Current Number and a Previous number
+
+ 
+for x in range (1,10):
+    print("Current: ", x)
+    Prev = x-1
+    print ("Previous: ", Prev)
+    sum = x  + Prev          
+    print("Sum:", sum)
